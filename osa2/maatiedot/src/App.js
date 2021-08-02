072ce7dbd3b81fa1cb1bce9d6a1158f832956a36
Tatu Hammarén <tatu.hammaren@gmail.com>
2021-08-02 13:56:04 +0300

    step1 done

--- osa2/maatiedot/src/App.js
+++ osa2/maatiedot/src/App.js
@@ -1,29 +1,46 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import Countries from './Components/Countries';
+//import Country from './Components/Country';
 
 
 const App = () => {
 const API_URL = "https://restcountries.eu/rest/v2/all";
-const [countries, setCountries] = useState([])
 
+const [countries, setCountries]  = useState( [ ] )
+const [filteredCountries, setFilteredCountries]  = useState( [ ] )
+const [search, setSearch] = useState( '' )
 
 useEffect(() =>{
-    axios
-    .get(API_URL)
-    .then(response => {
-      setCountries(response.data)
-    } )
-    
+  axios
+  .get( API_URL )
+  .then( response => {
+    setCountries(response.data)
+  } )
+  
 }, []);
 
-console.log(countries.length)
+const handleSearchChange = (e) => {
+ // console.log(e.target.value)
+  setSearch(e.target.value)
+  const filterCountries = countries
+                        .filter((country ) => 
+                        country.name.toLowerCase()
+                        .includes(search.toLowerCase()));
 
+  setFilteredCountries(filterCountries)
+}
 
   return (
-    <p>
-
-    </p>
+    <div>
+    <form>
+    <div>
+    find countries <input onChange={handleSearchChange} />
+    </div>
+    </form>
+    <Countries countries={filteredCountries} setCountries={setFilteredCountries} />
+    </div>
   );
-}
+  }
 
 export default App;
