67b3b6fc9300361ee5606ab834e2854ff9f6f452
Tatu Hammarén <tatu.hammaren@gmail.com>
2022-07-21 01:27:52 +0300

    fix newbook-form, missing half of the form because of cut paste

--- osa08/library-frontend/src/components/NewBook.js
+++ osa08/library-frontend/src/components/NewBook.js
@@ -39,6 +39,13 @@ const NewBook = (props) => {
     <div>
       <form onSubmit={submit}>
         <div>
+          title
+          <input
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
           author
           <input
             value={author}
@@ -53,6 +60,16 @@ const NewBook = (props) => {
             onChange={({ target }) => setPublished(target.value)}
           />
         </div>
+        <div>
+          <input
+            value={genre}
+            onChange={({ target }) => setGenre(target.value)}
+          />
+          <button onClick={addGenre} type="button">
+            add genre
+          </button>
+        </div>
+        <div>genres: {genres.join(' ')}</div>
         <button type="submit">create book</button>
       </form>
     </div>
